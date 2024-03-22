b659341838abf82ebb8e9c3c564b52d0e999eb1f
Talitha Shakira <tshakira20@gmail.com>
2024-03-22 19:20:06 +0700

    first commit

---
+++ LCD/LCD.java
@@ -0,0 +1,18 @@
+package LCD;
+
+public class LCD {
+    //isi bagian ini
+    
+    
+    //buat method yang ada pada soal 
+   
+
+    public void setCable(int option) {
+        //lengkapi method ini dengan mennggunakan array
+        
+    }
+    
+    public void displayMessage() {
+        //lengkapi method ini
+     
+}
